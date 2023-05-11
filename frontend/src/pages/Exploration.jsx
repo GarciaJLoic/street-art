a81e26caacb25ab68311967a3a721d53a6f15c8a
GarciaJLoic <98742928+GarciaJLoic@users.noreply.github.com>
2023-05-11 11:49:01 +0200

    Initialize 007Profil

---
+++ frontend/src/pages/Exploration.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header"
+import Map from "../components/Map"
+const Exploration = () => {
+  return (
+    <div className="exploration">
+      <Header />
+      <div className="exploration-title">
+        <h2>Exploration</h2>
+      </div>
+      <Map />
+    </div>
+  )
+}
+
+export default Exploration
